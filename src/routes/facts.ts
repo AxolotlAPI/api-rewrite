1b275bf232ace1a644b66b031294fe061dae3938
Ace <acekiron@outlook.com>
2022-07-01 05:06:05 +0000

    Optimized GetRandomFact() and Router.get("/")

--- src/routes/facts.ts
+++ src/routes/facts.ts
@@ -6,29 +6,45 @@ const FactsArray = Object.entries(FactsJSON);
 
 const Router = Express.Router();
 
-function GetRandomFact({ Author, Search }): String {
-    let fact;
+function GetRandomFact({ Author, Search, MinScore, MaxScore }): Object | null {
+    let NewFactsArray = FactsArray;
+    
+    if (Author) NewFactsArray = NewFactsArray.filter(fact => fact[1].author === Author);
+    if (Search) NewFactsArray = NewFactsArray.filter(fact => fact[1].content.includes(Search));
+    if (MinScore) NewFactsArray = NewFactsArray.filter(fact => fact[1].score >= MinScore);
+    if (MaxScore) NewFactsArray = NewFactsArray.filter(fact => fact[1].score <= MaxScore);
 
-    while (true) {
-        fact = FactsArray[Math.floor(Math.random() * FactsArray.length)];
+    if (NewFactsArray.length === 0) return null; // No fact was found
 
-        if (Author) if (fact.author != Author) continue;
-        if (Search) if (!fact.content.includes(Search)) continue;
-
-        break;
-    }
-
-    return fact[1];
+    let FactObj: any = NewFactsArray[Math.floor(Math.random() * NewFactsArray.length)];
+    let FactJson = FactObj[1];
+    FactJson.id = Number(FactObj[0]);
+    
+    return FactJson;
 }
 
 Router.get("/", (req, res) => {
-    const Amount = req.query.amount || 1; // Bulk searches
-    const Author = req.query.author; // Specific author
-    const Search = req.query.search; // Search for content
+    const Amount    = req.query.amount || 1; // Bulk searches
 
-    let Facts: Array<String> = [];
+    const Author    = req.query.author; // Specific author
+    const Search    = req.query.search; // Search for content
+    const MinScore  = req.query.minScore; // Minimum score
+    const MaxScore  = req.query.maxScore; // Maximum score
+
+    if (Amount > 100) return res.status(403).send({
+        message: "You're not allowed to request more than 100 facts at a time!"
+    });
+
+    let Facts: Array<Object> = [];
+
     for (let i = 0; i < Amount; i++) {
-        Facts.push(GetRandomFact({ Author, Search }));
+        let Fact: Object | null = GetRandomFact({ Author, Search, MinScore, MaxScore });
+        if (Fact !== null) Facts.push(Fact);
+        else {
+            return res.status(508).send({
+                message: `No facts could be found for query ${JSON.stringify(req.query)}.`
+            });
+        }
     }
 
     res.status(200).send(Facts);
